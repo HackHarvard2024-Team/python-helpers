8a0d3ec653e82757d233fb4d8b2e36d8ddf7d7f3
zedeckj <zedeck.j@northeastern.edu>
2024-10-13 03:27:37 -0400

    final data edits

--- analyze.py
+++ analyze.py
@@ -20,7 +20,7 @@ def parse(string):
 
 
 def make_blocks() -> dict[str, Block]:
-    with open("../usa.geojson", "r") as f:
+    with open("datafiles/usa.geojson", "r") as f:
         string = f.read()
         j = parse(string)
         features = j["features"] # type: ignore
@@ -43,18 +43,25 @@ addresses = {}
 
 def format_address(address : str, neighborhood : str):
     address += f", {neighborhood}, MA"
-    formatted = address.replace(" ", "+").replace("&", "and")   
+    formatted = address.replace(" ", "+").replace("&", "and").replace("\n", " ")  
     return formatted
 
 def address_to_coords(address : str, neighborhood : str):
+    print(address)
     formatted = format_address(address, neighborhood)
     if formatted in addresses:
-        return addresses[formatted]
+        print(formatted)
+        return
     # AIzaSyBuus780dlerTVuhGzjjq2Jg1HbzHECeZg
-    res = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?address={formatted}&key=AIzaSyBuus780dlerTVuhGzjjq2Jg1HbzHECeZg")
-    table = json.loads(res.text)
-    pos = table["results"][0]["geometry"]["viewport"]
-    addresses[formatted] = pos
+    try:
+        res = requests.get(f"https://maps.googleapis.com/maps/api/geocode/json?address={formatted}&key=AIzaSyBuus780dlerTVuhGzjjq2Jg1HbzHECeZg")
+        table = json.loads(res.text)
+        pos = table["results"][0]["geometry"]["viewport"]
+        addresses[formatted] = pos
+        print(formatted)
+    except:
+        print(f"error {formatted}")
+        return
 
 
 
@@ -122,9 +129,9 @@ def get_polygons_in(formatted : str, blocks : dict[str, Block], addresses : dict
     return out
 
 
-a = read_addresses()
 
 
+    
 
 def save_blocks_with_crimes():
     addresses = read_addresses()
@@ -151,18 +158,9 @@ def save_blocks_with_crimes():
                 blocks[poly_id].add_crime(description)
             i += 1
             print(i, end = "\r")
-            
+    
     pickle.dump(blocks, open('blocks', "wb"))
     
-with open("blocks", "rb") as f:
-    data = pickle.load(f)
-    print(len(data))
-    has = 0
-    for poly_id in data:
-        print(data[poly_id].crimes)
-        if len(data[poly_id].crimes) > 0:
-            has += 1
-    print(has)
     
 
 
@@ -172,9 +170,9 @@ client = OpenAI()
 def cosine_sim(v1, v2):
     return np.dot(v1,v2)/(norm(v1)*norm(v2))
 
-def get_embedding(text, model="text-embedding-3-large"):
+def get_embedding(text, model="text-embedding-3-small"):
    text = text.replace("\n", " ")
-   return np.array(client.embeddings.create(input = [text], model=model).data[0].embedding)
+   return np.array(client.embeddings.create(input = [text], model=model, dimensions = 256).data[0].embedding)
 
 
 CRIME_RANK2 = "murder rape arson aggravated assault first degree kidnapping human trafficking"
@@ -183,18 +181,30 @@ CRIME_RANK0 = "hugs smiles laughter joy good times clean fun smiley face"
 
 
 def make_crime_embeddings():
-    embeddings = {}
-    with open("../crime.csv", "r") as f:
-        lines = f.readlines()
-        for l in lines:
-            values = l.split(",")
-            description = values[3].replace('"', "")
-            if description not in embeddings:
-                embeddings[description] = get_embedding(description)
+    def inner(data2):
+        global x
+        for tup in data2:
+            if tup[1] not in embeddings:
+                embeddings[tup[1]] = get_embedding(tup[1])
+    embeddings = load_embeddings()
+    with open("add_crime_tups", "rb") as f:
+        data = pickle.load(f)
+    groups = []
+    inc = math.floor(len(data)/20)
+    for i in range(0, len(groups), inc):
+        groups.append(data[i : i + inc])
+    threads = []
+    for group in groups:
+        threads.append(threading.Thread(target = inner, args = (group)))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
     embeddings[CRIME_RANK2] = get_embedding(CRIME_RANK2)
     embeddings[CRIME_RANK1] = get_embedding(CRIME_RANK1)
     embeddings[CRIME_RANK0] = get_embedding(CRIME_RANK0)
     pickle.dump(embeddings, open("embeddings", "wb"))
+    return embeddings
 
 
     
@@ -202,12 +212,16 @@ def get_crime_emebedding(crime : str, embeddings):
     if crime in embeddings:
         return embeddings[crime]
     else:
-        get_embedding(crime)
+        return get_embedding(crime)
 
 
 
+scores = {}
 def crime_to_score(crime : str, embeddings):
+    global scores
     embedding = get_crime_emebedding(crime, embeddings)
+    if crime in scores:
+        return scores[crime]
     rank2 = get_crime_emebedding(CRIME_RANK2, embeddings)
     rank1 = get_crime_emebedding(CRIME_RANK1, embeddings)
     rank0 = get_crime_emebedding(CRIME_RANK0, embeddings)
@@ -218,17 +232,24 @@ def crime_to_score(crime : str, embeddings):
 
     sims = np.array([sim0, sim1, sim2])
 
-    return np.argmax(sims).item()
+    out = np.argmax(sims).item()
+    scores[crime] = out
+    return out
 
 def load_embeddings():
-    with open("embeddings", "rb") as f:
-        return pickle.load(f)
+    if "embeddings" in os.listdir("."):
+        with open("embeddings", "rb") as f:
+            return pickle.load(f)
+    else:
+        return {}
     
 
 def score_region(crimes : list[str], population : int, embeddings) -> float:
     out_score = 0.0
     for crime in crimes:
         out_score += crime_to_score(crime, embeddings)
+    if population == 0:
+        return 0
     out_score /= float(population)
     return out_score
 
@@ -238,10 +259,20 @@ def coords4_to_lat_longs(coords : list[tuple[float, float]]):
 
 
 
+
 def final_csv():
-    if "embeddings" not in os.listdir("."):
-        make_crime_embeddings()
-    embeddings = load_embeddings()
+    def inner(poly_ids, blocks, out_data2):
+        for poly_id in poly_ids:
+            block = blocks[poly_id]
+            if len(block.crimes) > 0:
+                score = score_region(block.crimes, block.population, embeddings)
+                simplified = block.polygon.simplify(4)
+                coords4 = simplified.getCoords()
+                out_line = [poly_id] + coords4_to_lat_longs(coords4) + [score]
+                out_data2.append(out_line)
+            print(f"{len(out_data2)}/{len(blocks)}", end = "\r")
+
+    embeddings = make_crime_embeddings()
     blocks = read_blocks()
     # blocks should already have crimes saved
     out_data = []
@@ -251,14 +282,22 @@ def final_csv():
         first_line.append(f"vertex{i}_lon")
     first_line.append("crime_score")
     out_data.append(first_line)
-    for poly_id in blocks:
-        block = blocks[poly_id]
-        if len(block.crimes) > 0:
-            score = score_region(block.crimes, block.population, embeddings)
-            simplified = block.polygon.simplify(4)
-            coords4 = simplified.getCoords()
-            out_line = [poly_id] + coords4_to_lat_longs(coords4) + [score]
-            out_data.append(out_line)
+    n = 0
+    groups = []
+    poly_ids = [p for p in blocks]
+    inc = math.floor(len(poly_ids) / 20)
+    threads = []
+    for x in range(0, len(poly_ids), inc):
+        groups.append(poly_ids[x : x + inc])
+    for group in groups:
+        threads.append(threading.Thread(target = inner, args = (group, blocks, out_data)))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+
+    """
     # Now, reescale calculated crime scores to 5 clusters using k-means
     model = KMeans(n_clusters = 5)
     array = np.array([row[-1] for row in out_data[1:]]).reshape(-1,1)
@@ -274,8 +313,195 @@ def final_csv():
         out_str += ",".join(line) + "\n"
     with open("final.csv", "w") as f:
         f.write(out_str)
+    """
 
 
 
-final_csv()
+def final_csv_fast():
+    def inner(poly_ids, blocks, out_data2):
+        for poly_id in poly_ids:
+            block = blocks[poly_id]
+            if len(block.crimes) > 0:
+                score = 1 if block.population == 0 else 1 + len(block.crimes) / (block.population ** 3)
+                simplified = block.polygon.simplify(4)
+                coords4 = simplified.getCoords()
+                out_line = [poly_id] + coords4_to_lat_longs(coords4) + [score]
+                out_data2.append(out_line)
+    
+            print(f"{len(out_data2)}/{len(blocks)}", end = "\r")
 
+    blocks = read_blocks()
+    # blocks should already have crimes saved
+
+    out_data = []
+    first_line = ["cluster_id"]
+    for i in range(4):
+        first_line.append(f"vertex{i}_lat")
+        first_line.append(f"vertex{i}_lon")
+    first_line.append("crime_score")
+    out_data.append(first_line)
+    n = 0
+    groups = []
+    poly_ids = [p for p in blocks]
+    inc = math.floor(len(poly_ids) / 20)
+    threads = []
+    for x in range(0, len(poly_ids), inc):
+        groups.append(poly_ids[x : x + inc])
+    for group in groups:
+        threads.append(threading.Thread(target = inner, args = (group, blocks, out_data)))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+
+
+
+
+
+    """
+    model = KMeans(n_clusters = 5)
+    array = np.array([row[-1] for row in out_data[1:]]).reshape(-1,1)
+    model.fit(array)
+    labels = model.predict(array)
+    print(labels)
+    print(len(labels))
+    print(len(out_data))
+
+
+    
+    for i in range(len(labels)):
+        out_data[i + 1][-1] = str(labels[i] + 1)  
+    """
+    out_data = [out_data[0]] + [row[0 : -1] + [row[-1]] for row in sorted(out_data[1:], key = lambda row : row[-1], reverse = True)]
+    scores = np.array([row[-1] for row in out_data[1:]])
+    # Heuristic for caluclating category
+    # Top 2% = 5
+    # Top 5% >= 4
+    # Top 10% >= 3
+    # Top 50% >= 2
+    # Bottom 50% = 1
+    threshold5 = len(out_data)/50
+    threshold4 = len(out_data)/20
+    threshold3 = len(out_data)/10
+    threshold2 = len(out_data)/2
+    def get_cat(i):
+        if i < threshold5:
+            return 5
+        elif i < threshold4:
+            return 4
+        elif i < threshold3:
+            return 3
+        elif i < threshold2:
+            return 2
+        else:
+            return 1
+    new_data = [out_data[0]]
+    for i in range(1,len(out_data)):
+        new_row = out_data[i][1:-1] + [str(get_cat(i))]
+        new_data.append(new_row)
+
+    out_data = new_data
+    out_str = ""
+    for line in out_data:
+        out_str += ",".join(line) + "\n"
+    out_str = ""
+    for line in out_data:
+        print(line)
+        out_str += ",".join(line) + "\n"
+    with open("datafiles/final_fast.csv", "w") as f:
+        f.write(out_str)
+
+
+addresses = {}
+def process_boston_add_group(data):
+    global addresses
+    for d in data:
+        if d["address"].strip() != "":
+            address_to_coords(d["address"], d["neighborhood"])
+
+def read_boston_crimes():
+    global addresses 
+    addresses = read_addresses()
+    print("BEFORE", len(addresses))
+    data = []
+    with open("datafiles/boston_crime.csv", "r") as f:
+        lines = f.readlines()
+        for l in lines[1:]:
+            l = l.split(",")
+            data.append({"address": l[5], "description": l[4].casefold(), "neighborhood": l[-1]})
+    groups = []
+    inc = math.floor(len(data)/10)
+    for i in range(0, len(data), inc):
+        groups.append(data[i : i + inc])
+    threads = []
+    for g in groups:
+        threads.append(threading.Thread(target = process_boston_add_group, args = (g,)))
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
+        
+
+    pickle.dump(addresses, open("addresses", "wb"))
+    print("AFTER", len(addresses))
+
+def make_full_crimes_tups():
+    addresses = read_addresses()
+    add_crime_tups = []
+    with open("datafiles/boston_crime.csv", "r") as f:
+        data = []
+        lines = f.readlines()
+        for l in lines[1:]:
+            l = l.split(",")
+            data.append({"address": l[5], "description": l[4].casefold().replace('"', ""), "neighborhood": l[-1]})
+        for d in data:
+            formatted = format_address(d["address"], d["neighborhood"])
+            add_crime_tups.append({"formatted": formatted, "crime": d["description"]})
+            
+    with open("datafiles/crime.csv") as f:
+        lines = f.readlines()
+        for l in lines[1:]:
+            values = l.split(",")
+            a = values[6].replace('"', "")
+            n = values[5].replace('"', "")
+            formatted = format_address(a, n)
+            description = values[3].replace('"', "")
+            add_crime_tups.append({"formatted": formatted, "crime": description})
+    pickle.dump(add_crime_tups, open("add_crime_tups", "wb"))
+
+
+
+def save_blocks_with_crimes_full():
+    def inner(data2):
+        for d in data2:
+            print(d)
+            if d["formatted"] not in addresses or d["formatted"] == ",+ ,+MA":
+                continue
+            poly_ids = get_polygons_in(d["formatted"], blocks, addresses)
+            description = d["crime"]
+            for poly_id in poly_ids:
+                blocks[poly_id].add_crime(description)
+
+    addresses = read_addresses()
+    blocks = make_blocks()
+    i = 0
+    with open("add_crime_tups", "rb") as f:
+        print("starting\n")
+        data = pickle.load(f)
+        groups = []
+        inc = math.floor(len(data)/10)
+        for n in range(0, len(data), inc):
+            groups.append(data[n : n + inc])
+        threads = []
+        for group in groups:
+            threads.append(threading.Thread(target = inner, args = (group,)))
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
+    
+    pickle.dump(blocks, open('blocks', "wb"))
+
+
+final_csv_fast()
